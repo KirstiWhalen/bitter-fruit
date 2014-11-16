2650cbdd0aad1a47ee49e21dda6819b1c229698a
Simon Whalen <simon.whalen@gmail.com>
2014-11-17 10:25:57 +1300

    Update

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
 
-    $('a[data-opens]').click(function() {
+    $('a[opens]').click(function() {
 
-        var openedby = $(this).attr('data-opens');
-        $('[data-openedby="' + openedby +'"]').removeClass('off').addClass('on');
+        var openedby = $(this).attr('opens');
+        $('[openedby="' + openedby +'"]').removeClass('off').addClass('on');
 
-        var closedby = $(this).attr('data-closes');
-        $('[data-closedby="' + closedby +'"]').css('display', 'none');
+        var closedby = $(this).attr('closes');
+        $('[closedby="' + closedby +'"]').css('display', 'none');
 
         $(this).addClass('clicked');
 
